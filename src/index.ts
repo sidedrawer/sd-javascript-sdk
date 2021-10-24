e43284748572cd670a41056063873f0db353ad37
matiasdodersidedrawer <mdoder@sidedrawerinc.com>
2021-10-24 19:21:50 -0300

    version 16

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import * as authservice from './services/auth-service';
+import * as networkservice from './services/network-service';
 
 
 export const createAuthSidedrawerClient = async (client_id: string) => {
     return await authservice.createAuthClient(client_id);
+};
+
+export const getSidedrawersOwned = async (token: string) => {
+    return networkservice.getOwned(token);
 };
 
--- src/services/network-service.ts
+++ src/services/network-service.ts
@@ -1,6 +1,6 @@
 import config from '../config.json';
 import axios from 'axios';
-import { Auth0Client } from '@auth0/auth0-spa-js';
+//import { Auth0Client } from '@auth0/auth0-spa-js';
 
 
 const instance = axios.create({
@@ -16,7 +16,7 @@ export const getShared = async () => {
     return instance.get(`sidedrawer/shared`);
 };
 
-export const getOwned = async (token: any) => {
+export const getOwned = async (token: string) => {
 
     return instance.get(`sidedrawer/owned`, { headers: { authorization: `Bearer ${token}` } });
 };
