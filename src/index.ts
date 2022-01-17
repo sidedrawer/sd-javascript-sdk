7b65da5f1bd763705dd40e4785cb9a08f5f80c90
Conrado Campetella <dev@ccampetella.com>
2022-01-17 02:10:03 -0300

    fix delete file method

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import {Environment} from "./models/environment.enum";
 import {AccountModule} from "./modules/account.module";
 import {RecordsModule} from "./modules/records.module";
 import {PlanRequestsModule} from "./modules/plan-requests.module";
+import {FilesModule} from "./modules/files.module";
 
 export class SdSdk {
     public SideDrawer = new SideDrawerModule(this.environment);
     public Account = new AccountModule(this.environment);
     public Records = new RecordsModule(this.environment);
     public PlanRequests = new PlanRequestsModule(this.environment);
+    public Files = new FilesModule(this.environment);
 
     constructor(
         private environment: Environment = Environment.production,
