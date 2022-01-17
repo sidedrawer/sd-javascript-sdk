fc28677a445318ed2b1e0f0533f408c935cabbd9
Conrado Campetella <dev@ccampetella.com>
2022-01-17 02:08:34 -0300

    add files modulew

--- src/helpers/utils.helper.ts
+++ src/helpers/utils.helper.ts
@@ -4,4 +4,27 @@ export class UtilsHelper {
             .filter(([_, v]) => v != null)
             .reduce((acc, [k, v]) => ({...acc, [k]: v}), {});
     }
+
+    public static generateCorrelationId(): string {
+        return new Date()
+            .toISOString()
+            .replaceAll('-','')
+            .replaceAll(':','')
+            .replaceAll('.','')
+            .trim();
+    }
+
+    public static generateFileName(name: string, hasExtension = true): string {
+        let formattedName = '';
+        if (hasExtension) {
+            const nameAux = name.split('.');
+            nameAux.splice(-1, 1);
+            formattedName = nameAux.join('.');
+        } else {
+            formattedName = name;
+        }
+        formattedName = formattedName.split(' ').join('_');
+        formattedName = formattedName.replace(/[^a-zA-Z0-9._%-]/g, '_');
+        return formattedName.replace(/%/g, '_');
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import {SideDrawerModule} from "./sidedrawers/sidedrawer.module";
+import {SideDrawerModule} from "./modules/sidedrawer.module";
 import {Environment} from "./models/environment.enum";
-import {AccountModule} from "./account/account.module";
-import {RecordsModule} from "./records/records.module";
+import {AccountModule} from "./modules/account.module";
+import {RecordsModule} from "./modules/records.module";
+import {PlanRequestsModule} from "./modules/plan-requests.module";
 
 export class SdSdk {
     public SideDrawer = new SideDrawerModule(this.environment);
     public Account = new AccountModule(this.environment);
     public Records = new RecordsModule(this.environment);
+    public PlanRequests = new PlanRequestsModule(this.environment);
 
     constructor(
         private environment: Environment = Environment.production,
