bdcf7aa413ecce8b1d1542abd0e558756442f44b
Leandro Brunner <lbrunner@sidedrawerinc.com>
2023-07-24 07:26:10 -0300

    Migrated from Parcel to Rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,66 @@
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+import pkg from "./package.json" assert { type: "json" };
+import replace from "@rollup/plugin-replace";
+
+const banner = `
+/**
+ * @license
+ * author: ${pkg.author}
+ * ${pkg.name}@${pkg.version}
+ * Released under the ${pkg.license} license.
+ */
+`;
+
+export default [
+  {
+    input: "./src/index.node.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs", // commonJS
+        banner,
+      },
+    ],
+    external: [...Object.keys(pkg.dependencies || {})],
+    plugins: [
+      typescript({
+        tsconfig: "./tsconfig.build.json",
+        sourceMap: false,
+      }),
+      nodeResolve({
+        browser: false,
+      }),
+      commonjs({ extensions: [".ts"] }),
+    ],
+  },
+  {
+    input: "./src/index.browser.ts",
+    output: [
+      {
+        name: "sidedrawer",
+        file: pkg.browser,
+        format: "umd", // browser
+        banner,
+      },
+    ],
+    plugins: [
+      typescript({
+        tsconfig: "./tsconfig.build.json",
+        sourceMap: false,
+      }),
+      nodeResolve({
+        browser: true,
+      }),
+      commonjs({ extensions: [".ts"] }),
+      replace({
+        "process.env.NODE_ENV": "browser",
+        preventAssignment: true,
+      }),
+      terser(),
+    ],
+  },
+];
